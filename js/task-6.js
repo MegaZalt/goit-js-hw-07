24b3583fe53c0840d2cfa5930a071a3918f04809
Valeriia <malvinacat0404@gmail.com>
2024-08-22 23:31:53 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,15 @@
-function getRandomHexColor() {
+window.onload = () => {
+
+  const input = document.querySelector('#number');
+  const buttonDestroy = document.querySelector('#destroy-button');
+  const buttonCreate = document.querySelector('#create-button');
+  const boxContainer = document.querySelector('#boxes');
+
+  
+
+  function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
 }
