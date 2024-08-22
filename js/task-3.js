2124219bc91a4160e7828792283508d09ff66f9d
Valeriia <malvinacat0404@gmail.com>
2024-08-22 21:50:52 +0200

    Update task-3.js

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input', function () {
+    const trimedName = this.value.trim();
+    nameOutput.textContent = trimedName || 'Anonymous';
+});
+
