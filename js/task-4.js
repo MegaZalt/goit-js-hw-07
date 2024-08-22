30c77d0e81dffa710adc71d65043df2b84f8ee8c
Valeriia <malvinacat0404@gmail.com>
2024-08-22 22:45:25 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
+window.onload = () => {
+
+    const form = document.querySelector('.login-form');
+
+    form.addEventListener('submit', handleSubmit);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const form = event.target;
+        const login = form.elements.login.value.trim();
+        const password = form.elements.login.value.trim();
+
+        if (!login || !password) {
+            alert('All form fields must be filled in');
+            return;
+        }
+
+        console.log('Login:', login);
+        console.log('Password:', password);
+
+        form.reset();
+
+    }
+}
+
 
