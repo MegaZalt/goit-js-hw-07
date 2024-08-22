dbcd337ea96e2303fcaadf1cbeb7a998f2d7ebd3
Valeriia <malvinacat0404@gmail.com>
2024-08-23 00:30:49 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,15 +1,48 @@
 window.onload = () => {
 
+  const controls = document.querySelector('#controls');
   const input = document.querySelector('#number');
   const buttonDestroy = document.querySelector('#destroy-button');
   const buttonCreate = document.querySelector('#create-button');
   const boxContainer = document.querySelector('#boxes');
 
-  
+
 
   function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-}
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  
+  function createBoxes(amount) {
+    boxContainer.innerHTML = "";
+
+    let boxSize = 30;
+
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.style.width = `${boxSize}px`;
+      box.style.height = `${boxSize}px`;
+      box.style.backgroundColor = getRandomHexColor();
+
+      boxContainer.appendChild(box);
+      boxSize += 10;
+    }
+  }
+
+  function destroyBoxes() {
+    boxContainer.innerHTML = "";
+  }
+
+  buttonCreate.addEventListener('click', () => {
+    const amount = parseInt(input.value, 10);
+
+    if (amount >= 1 && amount <= 100) {
+      createBoxes(amount);
+    }
+    input.value = "";
+  });
+
+  buttonDestroy.addEventListener('click', destroyBoxes);
+};
+
